1fcbf8f3b926d13b4e2098edb6fb882905288896
Luis Palacios <lrpalaciosdev@gmail.com>
2019-07-13 22:42:27 -0600

    add models and admin for question and choices

--- surveys/views.py
+++ surveys/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
+from .models import Question
 
 
 def index(request):
-    return render(request, "surveys/index.html")
+    question = Question.objects.all()[0]
+    return render(request, 'surveys/index.html', {
+        'question': question
+    })
